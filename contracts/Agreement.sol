075eec785c6a104f45e27f8ac6ada8a632e787ec
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-18 18:46:54 +0100

    Terms contract,WIP

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -1,69 +1,116 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import "hardhat/console.sol";
 
 contract Agreement {
- 
- //The issuer must create the agreement on ricardian fabric 
- address private issuer;
+    //The issuer must create the agreement on ricardian fabric
+    address public issuer;
 
- event NewTerms(string url, bytes32 termHash);
- event NewParticipant(address indexed participantAddress, bytes32 termHash,string proof);
+    event NewTerms(string url, bytes32 termHash);
+    event NewParticipant(address indexed participantAddress, string proof);
 
- // A string to store the url and the hash of the terms
- Terms public terms;
- 
- struct Terms{
-     string url;
-     bytes32 hash;
- }
- 
-  // key is the hash from the terms hashed with the agreeing address.
- mapping(bytes32 => Participant) agreements;
- 
- // The participant any wallet that accepts the terms.
- struct Participant {
-     bool signed;
-     string proof;
- }
- 
- 
-constructor(){
-    issuer = msg.sender;
-} 
- 
- // The setTerms allows an issuer to add new Term to their contract 
- function setTerms(string calldata _url,bytes32  _hash,uint8 _v, bytes32 _r, bytes32 _s) external {
-    // The signature is verified first
-    address signer = ecrecover(_hash,_v,_r,_s);
-    require(signer == issuer,"Invalid signature");
-    // If the issuer signature is detected, the terms can be updated
-    terms = Terms({
-        url: _url,
-        hash: _hash
-        
-    });
-    emit NewTerms(_url, _hash);
- }
- 
- // The accept function is called when a user accepts an agreement represented by the hash
- function accept(string calldata _url,bytes32 _hash,uint8 _v, bytes32 _r, bytes32 _s ) external {
-    require(terms.hash == _hash,"Invalid agreement");
-    address signer = ecrecover(_hash,_v,_r,_s);
-    require(signer == msg.sender,"Signer must be the sender");
-    bytes32 access = keccak256(abi.encodePacked(msg.sender,terms.hash));
-    agreements[access] = Participant({signed: true,proof: _url});
-    emit NewParticipant(msg.sender, _hash, _url);
- }
-// We can check if an address accepted the current terms or not
-function acceptedTerms(address _address) external view returns (bool){
-     bytes32 access = keccak256(abi.encodePacked(_address,terms.hash));
-     return agreements[access].signed;
- }
- 
- // The modifier allows a contract inheriting from this, to controll access easily based on agreement signing.
- modifier checkAcceptance{
-     bytes32 access = keccak256(abi.encodePacked(msg.sender,terms.hash));
-     require(agreements[access].signed,"You must accept the terms first..");
-     _;
- }
+    Terms terms;
+
+    struct Terms {
+        string url;
+        bytes32 hash;
+    }
+
+    // The key here is the hash from the terms hashed with the agreeing address.
+    mapping(bytes32 => Participant) agreements;
+
+    // The participant any wallet that accepts the terms.
+    struct Participant {
+        bool signed;
+        string proof;
+    }
+
+    constructor() {
+        issuer = msg.sender;
+    }
+
+    function verifySignature(
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        string calldata value,
+        string calldata url
+    ) public view returns (address) {
+        // require(value == terms.hash,"Invalid agreement");
+        uint256 chainId = 4;
+
+        bytes32 eip712DomainHash = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes("Ricardian Fabric")),
+                keccak256(bytes("1")),
+                chainId,
+                address(0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC)
+            )
+        );
+
+        bytes32 hashStruct = keccak256(
+            abi.encode(
+                keccak256(bytes("doc(string value,string url)")),
+                keccak256(bytes(value)),
+                keccak256(bytes(url))
+            )
+        );
+
+        bytes32 hash = keccak256(
+            abi.encodePacked("\x19\x01", eip712DomainHash, hashStruct)
+        );
+        address signer = ecrecover(hash, v, r, s);
+        return signer;
+    }
+
+    // The setTerms allows an issuer to add new Term to their contract
+    function setTerms(string calldata _url, bytes32 _hash)
+        external
+        returns (bool)
+    {
+        require(msg.sender == issuer, "Only the deployer can call this.");
+        // If the issuer signature is detected, the terms can be updated
+        terms = Terms({url: _url, hash: _hash});
+        emit NewTerms(_url, _hash);
+        return true;
+    }
+
+    function Accept(
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        string calldata value,
+        string calldata url
+    ) external {
+        address signer = verifySignature(v, r, s, value, url);
+        require(signer == msg.sender, "The signer must be the sender");
+        _accept(url);
+    }
+
+    // The accept function is called when a user accepts an agreement represented by the hash
+    function _accept(string calldata url) internal {
+        bytes32 access = keccak256(abi.encodePacked(msg.sender, terms.hash));
+        agreements[access] = Participant({signed: true, proof: url});
+        emit NewParticipant(msg.sender, url);
+    }
+
+    // We can check if an address accepted the current terms or not
+    function acceptedTerms(address _address) external view returns (bool) {
+        bytes32 access = keccak256(abi.encodePacked(_address, terms.hash));
+        return agreements[access].signed;
+    }
+
+    function getTerms() external view returns (string memory, bytes32) {
+        return (terms.url, terms.hash);
+    }
+
+    // The modifier allows a contract inheriting from this, to controll access easily based on agreement signing.
+    modifier checkAcceptance() {
+        bytes32 access = keccak256(abi.encodePacked(msg.sender, terms.hash));
+        require(agreements[access].signed, "You must accept the terms first..");
+        _;
+    }
 }
--- contracts/IAgreement.sol
+++ contracts/IAgreement.sol
@@ -3,39 +3,53 @@ pragma solidity ^0.8.0;
 
 // The agreement contract as called by Ricardian Fabric
 
+interface IAgreement {
+      event NewTerms(string url, bytes32 termHash);
+      event NewParticipant(address indexed participantAddress, string proof);
 
-interface IAgreement{
-
-
-   /**
+    /**
      * setTerms
      *
      * Adds new terms to the smart contract.
      * This is called by Ricardian Fabric when a new agreement is deployed.
-     * Should be only called by the issuer. 
-     * The _url is the deployed website's url, the hash is the precomputed hash of the agreement and the _v,_r,_s are the signature of the issuer.
+     * Should be only called by the issuer.
+     * T, the hash is the precomputed hash of the agreement and the _v,_r,_s are the signature of the issuer.
      */
-	function setTerms(string calldata _url,bytes32  _hash,uint8 _v, bytes32 _r, bytes32 _s) external;
+    function setTerms(
+        string calldata _url,
+        bytes32 _hash) external returns (bool);
 
-   /**
-     * accept
+    /**
+     * getTerms
+     *
+     *  You can get the terms for the contract by calling this function.
+     *
+     */
+    function getTerms() external view returns (string memory, bytes32);
+
+
+
+    /**
+     * Accept
      *
      * When a contract is signed in ricardian fabric, it calls this function with the signature
-     * 
-     * Should be only called by the issuer. 
      * The _url is the deployed website's url, the hash is the agreement's hash and the _v,_r,_s are the signature of the participant
      */
-	function accept(string calldata _url,bytes32 _hash,uint8 _v, bytes32 _r, bytes32 _s ) external;
-
-    
+    function Accept(
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        string calldata value,
+        string calldata url
+    ) external;
 
     /**
      * acceptedTerms
      *
-     * This external view function is used to verify if an address has accepted the agreement already. 
-     * 
+     * This external view function is used to verify if an address has accepted the agreement already.
+     *
      */
     function acceptedTerms(address _address) external view returns (bool);
 
-   
+
 }
