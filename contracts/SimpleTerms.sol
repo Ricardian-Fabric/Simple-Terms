965119a11d7b77e5aee9a8214c3d26d22064408d
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-10-18 19:17:30 +0100

    update terms object, value field

--- contracts/SimpleTerms.sol
+++ contracts/SimpleTerms.sol
@@ -6,14 +6,14 @@ contract SimpleTerms {
     //The issuer must create the agreement on ricardian fabric
     address public issuer;
 
-    event NewTerms(string url, bytes32 termHash);
+    event NewTerms(string url, string value);
     event NewParticipant(address indexed participantAddress, string proof);
 
     Terms terms;
 
     struct Terms {
         string url;
-        bytes32 hash;
+        bytes32 value;
     }
 
     // The key here is the hash from the terms hashed with the agreeing address.
@@ -36,7 +36,6 @@ contract SimpleTerms {
         string calldata value,
         string calldata url
     ) public pure returns (address) {
-        // require(value == terms.hash,"Invalid agreement");
         uint256 chainId = 4;
 
         bytes32 eip712DomainHash = keccak256(
@@ -67,14 +66,14 @@ contract SimpleTerms {
     }
 
     // The setTerms allows an issuer to add new Term to their contract
-    function setTerms(string calldata _url, bytes32 _hash)
+    function setTerms(string calldata _url, string calldata value)
         external
         returns (bool)
     {
         require(msg.sender == issuer, "Only the deployer can call this.");
         // If the issuer signature is detected, the terms can be updated
-        terms = Terms({url: _url, hash: _hash});
-        emit NewTerms(_url, _hash);
+        terms = Terms({url: _url, value: keccak256(abi.encodePacked(value))});
+        emit NewTerms(_url, value);
         return true;
     }
 
@@ -85,6 +84,12 @@ contract SimpleTerms {
         string calldata value,
         string calldata url
     ) external {
+        //The value signed must be the same as the terms value stored
+        require(
+            keccak256(abi.encodePacked(value)) == terms.value,
+            "Invalid terms"
+        );
+
         address signer = verifySignature(v, r, s, value, url);
         require(signer == msg.sender, "The signer must be the sender");
         _accept(url);
@@ -92,24 +97,24 @@ contract SimpleTerms {
 
     // The accept function is called when a user accepts an agreement represented by the hash
     function _accept(string calldata url) internal {
-        bytes32 access = keccak256(abi.encodePacked(msg.sender, terms.hash));
+        bytes32 access = keccak256(abi.encodePacked(msg.sender, terms.value));
         agreements[access] = Participant({signed: true, proof: url});
         emit NewParticipant(msg.sender, url);
     }
 
     // We can check if an address accepted the current terms or not
     function acceptedTerms(address _address) external view returns (bool) {
-        bytes32 access = keccak256(abi.encodePacked(_address, terms.hash));
+        bytes32 access = keccak256(abi.encodePacked(_address, terms.value));
         return agreements[access].signed;
     }
 
     function getTerms() external view returns (string memory, bytes32) {
-        return (terms.url, terms.hash);
+        return (terms.url, terms.value);
     }
 
     // The modifier allows a contract inheriting from this, to controll access easily based on agreement signing.
     modifier checkAcceptance() {
-        bytes32 access = keccak256(abi.encodePacked(msg.sender, terms.hash));
+        bytes32 access = keccak256(abi.encodePacked(msg.sender, terms.value));
         require(agreements[access].signed, "You must accept the terms first..");
         _;
     }
